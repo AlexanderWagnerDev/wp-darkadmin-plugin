bad4a7979576faa51abfa9d98b74a4d7c7509bb6
Alexander Wagner <github@alexanderwagnerdev.com>
2026-03-14 17:27:59 +0100

    Update darkadmin.php

--- darkadmin.php
+++ darkadmin.php
@@ -11,7 +11,7 @@
  * Author URI: https://alexanderwagnerdev.com
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain: darkadmin
+ * Text Domain: darkadmin-dark-mode-for-adminpanel
  * Domain Path: /languages
  */
 
